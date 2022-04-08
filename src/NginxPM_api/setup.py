3d76de2faea0783c742e74d0e39fad891ccd4b2b
N4v41 <n4v41.naval@gmail.com>
2022-04-07 22:07:14 -0300

    added setup.py

---
+++ src/NginxPM_api/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='NginxPM',
+      version='0.1',
+      description='A python module for interacting with Nginx Proxy Manager',
+      url='https://github.com/N4v41/NginxPM_api',
+      author='N4v41',
+      author_email='n4v41@naval.one',
+      license='MIT',
+      packages=['NginxPM'],
+      zip_safe=False)
